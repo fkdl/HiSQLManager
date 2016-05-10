62bf71affe9b3e7089c84a9fc2d254e2a9709687
XuminRong <xumingxsh21@126.com>
2016-05-10 10:58:24 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 修改测试文件名称

---
+++ HiCSSQL/test/UnitTestSQLProxy.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using HiCSSQL;
+
+namespace HiCSSQLTest
+{
+    [TestClass]
+    public class UnitTestSQLProxy
+    {
+        string folder = "";
+        public UnitTestSQLProxy()
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            DirectoryInfo topDir = System.IO.Directory.GetParent(path);
+            folder = topDir.FullName + "\\xml";
+            SQLProxy.SetLog(script =>
+            {
+                Trace.WriteLine(script);
+            });
+        }
+
+        [TestMethod]
+        public void LoadXMLs_Normal()
+        {
+            string path = folder + "\\sql";
+            SQLProxy.LoadXMLs(path);
+            Assert.IsTrue(SQLProxy.GetSqlInfo("DATA.COLLATIONS.GETDATASOURCE") != null);
+            SqlInfo info = SQLProxy.GetSqlInfo("DATA.INNODBLOCKS.GET8PAGE", (string propertyName, ref object objVal) =>
+            {
+                objVal = 3;
+                return true;
+            });
+            Assert.IsTrue(info != null);
+            Assert.IsTrue(string.IsNullOrWhiteSpace(info.CountSQL));
+        }
+        [TestMethod]
+        public void LoadXMLs_NoXMLFile()
+        {
+            string path = folder + "\\sqlNoXMLFile";
+            try
+            {
+                SQLProxy.LoadXMLs(path);
+                Assert.IsTrue(false);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        [TestMethod]
+        public void LoadXMLs_NoFolder()
+        {
+            string path = folder + "\\sqlNotExist";
+            try
+            {
+                SQLProxy.LoadXMLs(path);
+                Assert.IsTrue(false);
+            }
+            catch(Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        [TestMethod]
+        public void LoadXMLs_XMLError()
+        {
+            string path = folder + "\\sqlXmlError";
+            try
+            {
+                SQLProxy.LoadXMLs(path);
+                Assert.IsTrue(false);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+
+        [TestMethod]
+        public void LoadXMLs_NodeRepeate()
+        {
+            string path = folder + "\\sqlNodeRepeate";
+            try
+            {
+                SQLProxy.LoadXMLs(path);
+                Assert.IsTrue(false);
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+            }
+        }
+    }
+}
