b050ef74f736f2ba0d0f9e8bc75cdfc3c4349735
XuminRong <xumingxsh21@126.com>
2016-05-16 13:54:15 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 代码重构

--- HiCSSQL/SQL/SQLProxy.cs
+++ HiCSSQL/SQL/SQLProxy.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
-using System.Data.OleDb;
-using System.Data.SqlClient;
-using System.Data.OracleClient;
-using MySql.Data.MySqlClient;
 
 namespace HiCSSQL
 {
--- HiCSSQL/test/UnitTestSQLProxy.cs
+++ HiCSSQL/test/UnitTestSQLProxy.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Data;
 using System.IO;
 using System.Diagnostics;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
